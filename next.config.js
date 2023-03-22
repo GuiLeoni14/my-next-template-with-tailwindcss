483d491580c55cd31e1f008a226a24a2293eb3ef
GuiLeoni14 <guileone167@gmail.com>
2023-03-22 10:56:00 -0300

    feat: update next version and add page extensions

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
